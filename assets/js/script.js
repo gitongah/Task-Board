4e6158fc59441b94173e9b1ac895bfbe08df9165
gitongah <mbaabugitonga@gmail.com>
2024-04-04 07:38:35 -0700

    created the modal popup for adding the tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,36 +3,38 @@ let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
-function generateTaskId() {
+const generateTaskId = ()=> {
 
 }
 
 // Todo: create a function to create a task card
-function createTaskCard(task) {
+const createTaskCard = (task) => {
 
 }
 
 // Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {
+const renderTaskList = () => {
 
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
+const handleAddTask = (event) => {
 
 }
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
+const handleDeleteTask = (event) => {
 
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
-function handleDrop(event, ui) {
+const handleDrop = (event, ui) => {
 
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
-$(document).ready(function () {
+$(document).ready( () => {
+
+  renderTaskList();
 
 });
